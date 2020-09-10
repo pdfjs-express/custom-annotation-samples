f81e82f3eec4904581a2c220b903e0398859ac8d
Logan Bittner <lbittner@pdftron.com>
2020-09-10 09:03:53 -0700

    Index page and readme changes

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Custom annotation samples
 
 A repo containing examples on creating custom annotations in PDF.js Express.
+
+## Setup
+
+1) Install dependencies with `npm i`
+
+## Running
+
+1) Run `npm start`
+2) Go to http://localhost:3000
+
+## Table of contents
+
+- [Creating a triangle annotation](./samples/triangle-annotation)
+
+## References
+
+- [PDF.js Express](https://pdfjs.express)
+- [Custom annotation guide](https://pdfjs.express/documentation/annotation/custom-annotations)
--- server.js
+++ server.js
@@ -6,16 +6,30 @@ const app = express();
 
 const samplesBase = path.resolve(__dirname, './samples');
 const samples = fs.readdirSync(samplesBase);
-const htmlTemplate = fs.readFileSync(path.resolve(__dirname, './templates/sample.html')) + '';
 
 app.use('/samples', express.static('samples'))
 app.use('/static', express.static('static'))
 
 samples.forEach(sampleName => {
   app.get(`/${sampleName}`, (_, res) => {
+    const htmlTemplate = fs.readFileSync(path.resolve(__dirname, './templates/sample.html')) + '';
     const html = htmlTemplate.replace('{{SCRIPT_SRC}}', '/' + path.relative(__dirname, `./samples/${sampleName}/index.js`));
     res.send(html);
   })
+})
+
+app.get('/', (_, res) => {
+  const rootTemplate = fs.readFileSync(path.resolve(__dirname, './templates/index.html')) + '';
+
+  const html = samples.reduce((acc, sampleName) => {
+    return acc + `
+      <a href='/${sampleName}'>• ${sampleName}</a>
+    `
+  }, '')
+
+  const finalHTML = rootTemplate.replace('{{LINKS}}', html)
+
+  res.send(finalHTML);
 })
 
 app.listen(3000, () => {
